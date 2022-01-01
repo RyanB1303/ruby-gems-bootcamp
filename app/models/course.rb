efe4e94a5e496a34125f60d3fe8ae4f85bb6e165
RyanB1303 <popOs>
2022-01-01 20:33:13 +0700

    action_text gem - rich text editor not able yet to save rich type content

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,9 @@
 class Course < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true, length: {minimum:10, maximum:255}
+  def to_s
+    title
+  end
+
+  has_rich_text :description
 end
