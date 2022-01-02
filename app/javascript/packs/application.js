36d8cbe8d3432aee431c3c805c19959da7f3ef19
RyanB1303 <popOs>
2022-01-02 08:52:19 +0700

    fix bootstrap import in application.scss a bit of styling homepage and course page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,7 +14,6 @@ Turbolinks.start()
 ActiveStorage.start()
 
 import 'bootstrap/dist/js/bootstrap'
-import 'bootstrap/dist/css/bootstrap'
 import '@fortawesome/fontawesome-free/css/all'
 
 require("trix")
