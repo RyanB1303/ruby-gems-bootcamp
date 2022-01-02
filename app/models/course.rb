8d70fbe783fd9e5f91826628a947fee7bfad0772
RyanB1303 <popOs>
2022-01-03 02:53:46 +0700

    friendly_id gem

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,6 +3,15 @@ class Course < ApplicationRecord
   validates :description, presence: true, length: {minimum:10}
   has_rich_text :description
   belongs_to :user
+  extend FriendlyId
+  friendly_id :title, use: :slugged
+  # for randomly generated slug use this, e.g for transactional which we must protect user data
+  # friendly_id :generated_slug, use: :slugged
+
+  # def generated_slug
+  #   require 'securerandom'
+  #   @random_slug ||= persisted? ? friendly_id : SecureRandom.hex(4)
+  # end
 
   def to_s
     title
